f2d0e8673f52d38b49dbce8b72334519548be1db
kollawachhh <kollawach.s@ku.th>
2022-04-02 19:36:04 +0700

    Add logic

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueSweetalert2 from 'vue-sweetalert2';
 import router from "./router";
+
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 Vue.config.productionTip = false;
 
+Vue.use(VueSweetalert2);
 new Vue({
   router,
   render: h => h(App)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,32 @@
 <template>
   <div class="train">
     <template v-if="mode == 'train'">
-        <h1>เอาไรดี</h1>
+        <h2>โปรดทำใบหน้าที่แสดงอารมณ์ของท่านตามตัวเลือกด้านล่าง</h2>
+        <h1>{{emotion_choice}}</h1>
     </template>
-    <template v-else>
+    <template v-if="mode == 'home'">
+        <h1>Face Detection Checker</h1>
+    </template>
+    <template v-if="mode == 'test'">
         <h1>Take a picture to let us know how you feel about our service</h1>
+        <h1>{{randomChoice()}}</h1>
     </template>
-    <select id="use_case" v-on:change="changeOption()">
-        <option value="train">Train</option>
-        <option value="test">Test</option>
-    </select>
-    <Camera></Camera>
+    <Camera v-if="mode != 'home'"></Camera>
     <template v-if="mode == 'train'">
-        <select id="emotion_options">
-            <template v-for="(emotion, index) in emotions">
-                <option :key="index" :value="index">{{emotion}}</option>
-            </template>
-        </select>
-        <button v-on:click="trainModel()">Train Model</button>
+        <button v-on:click="trainModel()">ยืนยันใบหน้า</button>
+        <h3>กรุณายืนยันใบหน้าหลายๆรอบในแต่ละอารมณ์</h3>
+    </template>
+    <template v-if="mode == 'home'">
+        <button @click="changeOption('train')">Start</button>
     </template>
     <template v-else>
-        <button v-on:click="getEmotion()">Get Emotion</button>
+        <button v-if="mode == 'test'" v-on:click="getEmotion()">ตรวจจับใบหน้า</button>
+        <button v-if="mode == 'train' && count.angry == 0" @click="changeOption('test')">เริ่มเล่น</Button> 
+        <button disabled v-if="mode == 'train' && count.angry != 0" @click="changeOption('test')">เริ่มเล่น</Button> 
+        <button v-if="mode != 'home'" @click="changeOption('home')">หน้าหลัก</button>
     </template>
 
-    <h1>{{ detected_e }}</h1>
+    <h1 v-if="mode == 'test'">{{ detected_e }}</h1>
   </div>
 </template>
 
@@ -42,12 +45,18 @@ export default {
   },
   data: function(){
       return {
-          emotions: ['angry','neutral', 'happy'],
+          emotions: ['happy','neutral', 'angry'],
+          emotion_choice: 'happy',
           classifier: null,
           mobilenet: null,
           class: null,
           detected_e: null,
-          mode: 'train',
+          mode: 'home',
+          count:{
+            happy: 3,
+            neutral: 3,
+            angry: 3,
+          },
       }
   },
   mounted: function(){
@@ -59,9 +68,30 @@ export default {
         this.classifier = knnClassifier.create();
         this.mobilenet = await mobilenetModule.load();
       },
+      prepareModel(){
+      
+      },
       trainModel(){
-        let selected = document.getElementById("emotion_options");
-        this.class = selected.options[selected.selectedIndex].value;
+        if(this.count.happy > 0){
+          this.count.happy -= 1
+          if(this.count.happy == 0){
+            this.emotion_choice = 'neutral'
+          }
+          this.class = 0
+        }
+        else if(this.count.neutral > 0){
+          this.emotion_choice = 'neutral'
+          this.count.neutral -= 1
+          this.class = 1
+        }
+        else if(this.count.angry > 0){
+          this.emotion_choice = 'angry'
+          this.count.angry -= 1
+          this.class = 2
+        }
+        console.log(this.class)
+        // let selected = document.getElementById("emotion_options");
+        // this.class = selected.options[selected.selectedIndex].value;
         this.addExample();
       },
       addExample(){
@@ -74,12 +104,22 @@ export default {
         const logits = this.mobilenet.infer(img, 'conv_preds');
         const pred = await this.classifier.predictClass(logits);
         console.log(pred);
-        this.detected_e = this.emotions[pred.classIndex];
+        if(this.emotions[pred.classIndex] == this.emotion_choice){
+          this.detected_e = "correct"
+        }
+        else{
+          this.detected_e = "incorrect"
+        }
+        // this.detected_e = this.emotions[pred.classIndex];
+        this.randomChoice();
         this.registerEmotion();
       },
-      changeOption(){
-          const selected = document.getElementById("use_case");
-          this.mode = selected.options[selected.selectedIndex].value;
+      randomChoice() {
+        this.emotion_choice = this.emotions[Math.floor(Math.random() * this.emotions.length)];
+        return this.emotion_choice
+      },
+      changeOption(mode){
+        this.mode = mode;
       },
       registerEmotion(){
           axios.post('http://localhost:3128/callback', {
